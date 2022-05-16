9b1b3981f89d841809751d5f0aa5cb85da32a6b2
张晓瑞 <zxr35l@163.com>
2022-05-17 00:12:19 +0800

    Add project structure.

---
+++ service/authority-service/src/main/java/xdu/cloudnative/service/authority/AuthorityApplication.java
@@ -0,0 +1,11 @@
+package xdu.cloudnative.service.authority;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthorityApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AuthorityApplication.class,args);
+    }
+}
